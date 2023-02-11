b7ecf38ff0167ce44cd52ead677dcadf69e9e31c
JinjuNa <jinju@liveklass.com>
2023-02-11 17:54:07 +0900

    4.5 Custom Tab Bar

--- lib/features/main_navigation.dart/main_navigation_screen.dart
+++ lib/features/main_navigation.dart/main_navigation_screen.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation.dart/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -29,6 +32,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   ];
 
   void _onTap(int index) {
+    print(11111);
     setState(() {
       _selectedIndex = index;
     });
@@ -36,22 +40,44 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            CupertinoIcons.home,
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            Sizes.size12,
           ),
-          label: 'home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            CupertinoIcons.search,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: 'Home',
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: 'Discover',
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: 'Inbox',
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: 'Profile',
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          label: 'search',
         ),
-      ]),
-      tabBuilder: (context, index) => screens[index],
+      ),
     );
   }
 }
