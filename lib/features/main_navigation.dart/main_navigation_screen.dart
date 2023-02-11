014f97e433bbd4757dd7aaf23855c2fca883e9ef
JinjuNa <jinju@liveklass.com>
2023-02-11 17:23:33 +0900

    4.4 CupertinoTabBar

--- lib/features/main_navigation.dart/main_navigation_screen.dart
+++ lib/features/main_navigation.dart/main_navigation_screen.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -37,19 +36,22 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: const [
-          NavigationDestination(
-              icon: FaIcon(FontAwesomeIcons.house), label: 'home'),
-          NavigationDestination(
-              icon: FaIcon(FontAwesomeIcons.magnifyingGlass), label: 'search'),
-        ],
-      ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.home,
+          ),
+          label: 'home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.search,
+          ),
+          label: 'search',
+        ),
+      ]),
+      tabBuilder: (context, index) => screens[index],
     );
   }
 }
